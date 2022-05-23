8acaa7667a055d9e06267e8579432f791d30df30
NickAstley <andreanicastro1994@gmail.com>
2022-05-23 18:46:09 +0200

    Added extra and super extra bonuses

--- Coda Array - Bonus/js/main.js
+++ Coda Array - Bonus/js/main.js
@@ -3,23 +3,32 @@ Creare un array con un numero a piacere di elementi.
 Stampare a schermo, con un ciclo, gli ultimi 5 elementi dell’array.
 Attenzione #1: quanti elementi minimo dovrà contenere l’array?
 Attenzione #2: il ciclo deve essere costruito per estrarre gli ultimi elementi da qualsiasi array, 
-anche se non conosciamo a priori quanti elementi contiene. */
+anche se non conosciamo a priori quanti elementi contiene. 
+
+Extra Bonus
+Creiamo un array chiedendo all’utente quanti elementi dovrà contenere e generando tanti numeri interi casuali 
+quanti sono gli elementi da inserire.
+Attenzione #3: quali controlli dobbiamo fare sull’input dell’utente?
+Super Extra Bonus: chiedere all’utente anche quanti elementi vanno estratti dal fondo dell’array. */
 
 // Prendo il bottone di invio
 const submitButton = document.getElementById("submitButton");
 
 // Aggiungo un Event Listener sul bottone
 submitButton.addEventListener("click", function() {
-    // Prendo il numero specificato dall'utente
+    // Prendo i numeri specificato dall'utente
     const arrayLength = parseInt(document.getElementById("arrayLength").value);
+    const displayNumber = parseInt(document.getElementById("displayNumber").value);
 
     // Prendo uno span dove specificare i numeri e ne resetto il contenuto
     const arrayEnd = document.getElementById("arrayEnd");
     arrayEnd.innerHTML = "";
 
-    // Controllo che sia stata specificata una lunghezza di almeno 5
-    if (arrayLength < 5) {
-        arrayEnd.innerHTML = "I numeri devono essere almeno 5!";
+    // Controllo che sia stata specificata una lunghezza coerente al numero di elementi da mostrare
+    if (displayNumber <= 0) {
+        arrayEnd.innerHTML = "Inserisci un numero maggiore di 0!";
+    } else if (arrayLength < displayNumber) {
+        arrayEnd.innerHTML = "I numeri casuali non sono abbastanza per essere mostrati!";
     } else {
         // Creo l'array
         const randomNumbers = [];
@@ -30,7 +39,7 @@ submitButton.addEventListener("click", function() {
         }  
         console.log(randomNumbers);
 
-        for (let i = randomNumbers.length - 5; i < randomNumbers.length; i++) {
+        for (let i = randomNumbers.length - displayNumber; i < randomNumbers.length; i++) {
             arrayEnd.innerHTML += `${randomNumbers[i]} `;
         }
     }
